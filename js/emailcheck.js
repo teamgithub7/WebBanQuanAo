3802c4290d41851a342811fcd5d27da56708d8a4
teamgithub7 <tuandz151102@gmail.com>
2022-11-26 21:47:42 +0700

    Update emailcheck.js

--- js/emailcheck.js
+++ js/emailcheck.js
@@ -1,5 +1,5 @@
 function checkemail() {
-    var email = document.getElementById('email').value;
+    var email = document.getElementById('email');
     var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     if (!filter.test(email.value)) {
         alert("nhập địa chỉ email hợp lệ. vd example@gmail.com");
